cddfa2e967843029e49d6cd0495aa049f77ca682
Anderson Choren <andersonchoren@gmail.com>
2022-04-14 10:58:01 -0300

    Exercício conversor de temperatura

---
+++ Conversor de temperatura/conversor.php
@@ -0,0 +1,43 @@
+<?php
+session_start();
+if (!$_GET["temperature"] || !$_GET["txtCelsius"]) {
+    $_SESSION["msg_error"] = "Requisição inválida!!!";
+    header("location:index.php");
+}
+
+$celsius = $_GET["txtCelsius"];
+$result = 0;
+
+switch ($_GET["temperature"]) {
+    case "Fahrenheit":
+        $result = toFahrenheit($celsius);
+        $_SESSION["msg_value"] = "$celsius °C para Fahrenheit é $result";
+        break;
+    case "Kelvin":
+        $result = toKelvin($celsius);
+        $_SESSION["msg_value"] = "$celsius °C para Kelvin é $result";
+        break;
+    case "Rankine":
+        $result = toRankine($celsius);
+        $_SESSION["msg_value"] = "$celsius °C para Rankine é $result";
+        break;
+    default:
+        $_SESSION["msg_error"] = "A temperatura para conversão é inválida!!!";
+}
+header("location:index");
+
+// Funções de conversão
+function toFahrenheit($celsius)
+{
+    return $celsius * 1.8 + 32;
+}
+
+function toKelvin($celsius)
+{
+    return $celsius + 273.15;
+}
+
+function toRankine($celsius)
+{
+    return $celsius * 1.8 + 491.67;
+}
---
+++ Conversor de temperatura/index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Conversor de temperatura</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+</head>
+
+<body>
+    <main class="container">
+        <?php
+        session_start();
+        if (!empty($_SESSION["msg_error"])) :
+        ?>
+            <div class="alert alert-warning">
+                <?= $_SESSION["msg_error"] ?>
+            </div>
+        <?php endif; unset($_SESSION["msg_error"]) ?>
+        
+        <form action="conversor.php" method="get">
+            <section class="row">
+                <article class="col-12 col-md-6">
+                    <div class="mb-3">
+                        <label for="txtCelsius" class="form-label">Temperatura em °C</label>
+                        <input type="number" name="txtCelsius" id="txtCelsius" class="form-control" required pattern="[0-9]{1,}">
+                    </div>
+                </article>
+                <article class="col-12 col-md-6">
+                    <div class="form-check">
+                        <label for="txtFahrenheit" class="form-check-label">Fahrenheit</label>
+                        <input type="radio" name="temperature" id="txtFahrenheit" class="form-check-input" value="Fahrenheit">
+                    </div>
+                    <div class="form-check">
+                        <label for="txtKelvin" class="form-check-label">Kelvin</label>
+                        <input type="radio" name="temperature" id="txtKelvin" class="form-check-input" value="Kelvin">
+                    </div>
+                    <div class="form-check">
+                        <label for="txtRankine" class="form-check-label">Rankine</label>
+                        <input type="radio" name="temperature" id="txtRankine" class="form-check-input" value="Rankine">
+                    </div>
+                </article>
+            </section>
+            <section class="row">
+                <article class="col">
+                    <div class="d-flex justify-content-center">
+                        <button type="submit" class="btn btn-primary">Converter</button>
+                    </div>
+                </article>
+            </section>
+        </form>
+    </main>
+</body>
+
+</html>
