ab1c41cc4921054c7793440e634b64dccf94eabb
Anderson Choren <andersonchoren@gmail.com>
2022-05-05 11:59:25 -0300

    Projeto empresa - User Model

--- Empresa/src/Controller/User.php
+++ Empresa/src/Controller/User.php
@@ -2,10 +2,47 @@
 
 namespace APP\Controller;
 
-use APP\Model\Connection;
+use APP\Model\Validation;
 
 require_once "../../vendor/autoload.php";
 
-$connection = Connection::getConnection();
+session_start();
+if (empty($_POST)) {
+    $_SESSION["msg_error"] = "Requisição inválida";
+    header("../View/form_user_add.php");
+}
 
-var_dump($connection);
+$error = array();
+
+if (empty($_POST["login"])) {
+    array_push($error, "Campo de login não localizado!!!");
+}
+
+if (empty($_POST["password"])) {
+    array_push($error, "Campo de senha não localizado!!!");
+}
+
+redirect($error);
+
+$login = $_POST["login"];
+$password = $_POST["password"];
+
+if (!Validation::validateLogin($login)) {
+    array_push($error, "Login deve conter no mínimo 8 caracteres entre números e letras");
+}
+
+if (!Validation::validatePassword($password)) {
+    array_push($error, "Senha deve conter no mínimo 8 caracteres entre números e letras");
+}
+
+redirect($error);
+
+// Modelo do User
+
+function redirect(array $error, string $location = "../View/Message.php")
+{
+    if (count($error) > 0) {
+        $_SESSION["msg_verify_error"] = $error;
+        header("location:$location");
+    }
+}
---
+++ Empresa/src/Model/Validation.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace APP\Model;
+
+class Validation
+{
+    public static function validateLogin(string $login):bool
+    {
+        return strlen($login) > 7;
+    }
+
+    public static function validatePassword(string $password):bool
+    {
+        return strlen($password) > 7;
+    }
+}
